fe5ed53ff29e7cdcc24ba229e3b0bd02c7376bb2
Marcos Zahonero <maek0szaho@gmail.com>
2025-02-04 19:54:53 +0100

    Categoria juegos metida

--- Pages/LoginSystemPowerCode/LoginSystemPowerCode/obj/Debug/net8.0-android/LoginSystemPowerCode.AssemblyInfo.cs
+++ Pages/LoginSystemPowerCode/LoginSystemPowerCode/obj/Debug/net8.0-android/LoginSystemPowerCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginSystemPowerCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+677a629ed08854090d13d5e3bf38b9e9e68c2d37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d30e13ffc878250327da89fa50f836ee74c7b942")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginSystemPowerCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginSystemPowerCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
