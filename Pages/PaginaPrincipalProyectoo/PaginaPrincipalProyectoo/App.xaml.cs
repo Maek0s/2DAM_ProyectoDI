6f6273e636ecdabb3d4b141553a315462f1848ba
izancluac <153533514+izancluac@users.noreply.github.com>
2025-02-15 14:35:20 +0100

    Add files via upload
    Pagina  Principal update 1

---
+++ Pages/PaginaPrincipalProyectoo/PaginaPrincipalProyectoo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PaginaPrincipalProyectoo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Pages/PaginaPrincipalProyectoo/PaginaPrincipalProyectoo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PaginaPrincipalProyectoo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
